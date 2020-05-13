350545016c21a7f443f9ae4021afaa7aba73243f
Kannan Pradeepkumar <kannanpradeepkumar1@gmail.com>
2020-05-13 23:20:08 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.grey[900],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.yellow,
+                backgroundImage: AssetImage('images/batman_face.png'),
+              ),
+              Text(
+                'Batman',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 40.0,
+                    color: Colors.grey[300],
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Detective'.toUpperCase(),
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.grey[600],
+                ),
+              ),
+              Card(
+                  color: Colors.grey[600],
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.grey[900],
+                    ),
+                    title: Text(
+                      '+11 12 1231235',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        color: Colors.grey[900],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )),
+              Card(
+                color: Colors.grey[600],
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.grey[900],
+                  ),
+                  title: Text(
+                    'batman@batcave.com',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      color: Colors.grey[900],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
